dfa07a49c44d1d401832e4f56d166f4f8f844412
niksavellano <avellanonikko@gmail.com>
2022-03-17 13:20:47 -0600

    added login page

--- chronus/src/Component/Index.jsx
+++ chronus/src/Component/Index.jsx
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 
 class Index extends Component {
   render() {
-    return <div>Login Page</div>;
+    return (
+      <div>
+        <form action="">
+          <label htmlFor="">
+            Username
+            <input type="text" name="login" id="login" />
+          </label>
+        </form>
+      </div>
+    );
   }
 }
 export default Index;
