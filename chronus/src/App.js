38e3813d9c5535bfe58d765d3367d2f7d643f575
niksavellano <avellanonikko@gmail.com>
2022-03-17 13:44:59 -0600

    added new login form

--- chronus/src/App.js
+++ chronus/src/App.js
@@ -1,6 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Index from "./Component/Index";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 function App() {
   return (
--- chronus/src/Component/Index.jsx
+++ chronus/src/Component/Index.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
+import titan from "../pics/Titan.png";
+import "./css/Index.css";
 
 class Index extends Component {
   render() {
     return (
-      <div>
-        <form action="">
-          <label htmlFor="">
-            Username
-            <input type="text" name="login" id="login" />
-          </label>
-        </form>
+      <div className="container">
+        <img src={titan} alt="" className="titan" />
+        <div className="form-floating mb-3">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Email</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="password"
+            class="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
+        <button type="button" class="btn btn-success">
+          Login
+        </button>
+        <br />
+        <button type="button" class="btn btn-success">
+          Guest
+        </button>
       </div>
     );
   }
